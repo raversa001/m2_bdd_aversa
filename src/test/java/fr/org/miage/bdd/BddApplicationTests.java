355cc083c98e854c0175001504fba65c5680b217
raversa001 <richard.aversa@etu.unice.fr>
2024-02-22 14:03:46 +0100

    Initial commit

---
+++ src/test/java/fr/org/miage/bdd/BddApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.org.miage.bdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
