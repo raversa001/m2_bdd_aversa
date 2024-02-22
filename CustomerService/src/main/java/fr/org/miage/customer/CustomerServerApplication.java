a1e65bd78dd2a47bb6791d6b8551f4b53a65ebb6
raversa001 <richard.aversa@etu.unice.fr>
2024-02-22 15:13:25 +0100

    Done with boilerplate
    /

---
+++ CustomerService/src/main/java/fr/org/miage/customer/CustomerServerApplication.java
@@ -0,0 +1,13 @@
+package fr.org.miage.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServerApplication.class, args);
+    }
+
+}
---
+++ CustomerService/src/test/java/fr/org/miage/customer/CustomerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.org.miage.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
