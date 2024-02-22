8bf5bd25083b0d89730d8aed1e0ddd6ee1523f9e
raversa001 <richard.aversa@etu.unice.fr>
2024-02-22 16:38:45 +0100

    Update Account.java
    /

--- Account/src/main/java/fr/org/miage/account/entities/Account.java
+++ Account/src/main/java/fr/org/miage/account/entities/Account.java
@@ -1,6 +1,16 @@
 package fr.org.miage.account.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
+@Entity
 public class Account {
+
+    @Id
+    private Long id;
 
 }
