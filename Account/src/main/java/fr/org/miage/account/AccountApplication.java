8509b4a1089e1a3d946038e0308ae9d5c704a52c
raversa001 <richard.aversa@etu.unice.fr>
2024-02-23 09:23:40 +0100

    Update AccountApplication.java
    /

--- Account/src/main/java/fr/org/miage/account/AccountApplication.java
+++ Account/src/main/java/fr/org/miage/account/AccountApplication.java
@@ -2,6 +2,7 @@ package fr.org.miage.account;
 
 import fr.org.miage.account.entities.Account;
 import fr.org.miage.account.enums.CurrencyType;
+import fr.org.miage.account.proxies.CustomerProxy;
 import fr.org.miage.account.repositories.AccountRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,22 +16,26 @@ import java.time.LocalDate;
 @SpringBootApplication
 public class AccountApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
 
-	@Bean
-	CommandLineRunner commandLineRunner(AccountRepository accountRepository) {
-		return args -> {
-			Account acc1 = Account
-					.builder()
-					.dateCreated(LocalDate.now())
-					.balance(Math.random() * 34000)
-					.currencyType(CurrencyType.EUR)
-					.customerId(1L)
-					.build();
+    @Bean
+    CommandLineRunner commandLineRunner(AccountRepository accountRepository, CustomerProxy customerProxy) {
+        return args -> {
+            customerProxy.getAllCustomers().forEach(
+                    customer -> {
+                        Account acc1 = Account
+                                .builder()
+                                .dateCreated(LocalDate.now())
+                                .balance(Math.random() * 34000)
+                                .currencyType(CurrencyType.EUR)
+                                .customerId(1L)
+                                .build();
 
-			accountRepository.save(acc1);
-		};
-	}
+                        accountRepository.save(acc1);
+                    }
+            );
+        };
+    }
 }
