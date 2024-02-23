67136c49346246f58a70a09982082a90eeced790
raversa001 <richard.aversa@etu.unice.fr>
2024-02-23 12:03:01 +0100

    Update CustomerProxy.java
    /

--- Account/src/main/java/fr/org/miage/account/proxies/CustomerProxy.java
+++ Account/src/main/java/fr/org/miage/account/proxies/CustomerProxy.java
@@ -1,6 +1,7 @@
 package fr.org.miage.account.proxies;
 
 import fr.org.miage.account.models.Customer;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,8 +11,18 @@ import java.util.List;
 @FeignClient(name = "customer", url = "http://localhost:8091/")
 public interface CustomerProxy {
     @GetMapping("/customer/{id}")
+    @CircuitBreaker(name = "customer", fallbackMethod = "getDefaultCustomer")
     public Customer getCustomerById(@PathVariable Long id);
 
     @GetMapping("/customers")
+    @CircuitBreaker(name = "customer", fallbackMethod = "getAllCustomers")
     public List<Customer> getAllCustomers();
+
+    default Customer getDefaultCustomer(Long id, Exception exception) {
+        return new Customer(id, "Not available", "Not available", "Not available");
+    }
+
+    default List<Customer> getAllCustomers(Exception exception) {
+        return List.of();
+    }
 }
